9d3ccb3cee0cfc275fc90690043b6da749c3e022
Vitor Souza <104155312+NerdVitro@users.noreply.github.com>
2023-08-29 22:05:59 -0300

    Commit

---
+++ TestePerformace/ClasseTeste.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestePerformace
+{
+    public class ClasseTeste
+    {
+        private int _IDDOCARA;
+        private string _NOMECARA;
+
+        public int IDDOCARA { get => _IDDOCARA; set => _IDDOCARA = value; }
+        public string NOMECARA { get => _NOMECARA; set => _NOMECARA = value; }
+
+        public ClasseTeste(int iDDOCARA, string nOMECARA)
+        {
+            _IDDOCARA = iDDOCARA;
+            _NOMECARA = nOMECARA;
+        }
+        public ClasseTeste()
+        {
+
+        }
+    }
+}
---
+++ TestePerformace/Program.cs
@@ -0,0 +1,50 @@
+﻿using TestePerformace;
+
+Console.WriteLine("Programa começou");
+List<ClasseTeste> ObjetoLista = new();
+
+DateTime dataInicio = DateTime.Now;
+for (int i = 0; i <= 1000000; i++)
+{
+    ObjetoLista.Add(new ClasseTeste(
+        i,
+        i.ToString()
+        ));
+}
+DateTime dataFim = DateTime.Now;
+TimeSpan timeSpan = dataFim - dataInicio;
+Console.WriteLine("Preencheu os dados");
+Console.WriteLine("Demorou: " + timeSpan.TotalSeconds.ToString());
+
+List<ClasseTeste> ObjetoListaNumPar = new();
+List<ClasseTeste> ObjetoListaNumImpar = new();
+
+dataInicio = DateTime.Now;
+foreach (ClasseTeste classe in ObjetoLista)
+{
+    bool blxIsPar = classe.IDDOCARA % 2 == 0;
+    switch (blxIsPar)
+    {
+        case true:
+            ObjetoListaNumPar.Add(classe);
+            break;
+        case false:
+            ObjetoListaNumImpar.Add(classe);
+            break;
+    }
+}
+
+dataFim = DateTime.Now;
+timeSpan = dataFim - dataInicio;
+Console.WriteLine("Demorou pra filtrar 1: " + timeSpan.TotalSeconds.ToString());
+
+dataInicio = DateTime.Now;
+ObjetoListaNumPar = new();
+ObjetoListaNumImpar = new();
+
+ObjetoListaNumPar = ObjetoLista.Where(x => x.IDDOCARA % 2 == 0).ToList();
+ObjetoListaNumImpar = ObjetoLista.Where(x => x.IDDOCARA % 2 != 0).ToList();
+
+dataFim = DateTime.Now;
+timeSpan = dataFim - dataInicio;
+Console.WriteLine("Demorou pra filtrar 2: " + timeSpan.TotalSeconds.ToString());
